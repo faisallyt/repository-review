5a2db4f8a3c814cee9428fc910da1223bbcf7de1
faisallyt <fdiledu@gmail.com>
2024-08-05 03:17:59 +0530

    project initialized

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express=require('express');
+const app=express();
+const dotenv=require("dotenv");
+const cors=require("cors");
+const cookieparser=require("cookie-parser");
+
+app.use(express.json());
+
+app.use(cors({
+    origin:"*",
+    credentials:true,
+}))
+
+
+dotenv.config({
+    path: "./.env",
+})
+
+app.listen(8080,()=>{
+    console.log("listening on",8080);
+})

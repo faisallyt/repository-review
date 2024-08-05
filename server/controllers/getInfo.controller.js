659e9e546d2cc60fcfbf3f5d58467e362dc135f5
faisallyt <fdiledu@gmail.com>
2024-08-06 02:33:36 +0530

    folder Structure generated

--- server/controllers/getInfo.controller.js
+++ server/controllers/getInfo.controller.js
@@ -1,32 +1,30 @@
-    const { asyncHandler } = require("../utils/asyncHandler.utils.js");
-    const { ApiError } = require("../utils/ApiError.utils.js");
-    const { ApiResponse } = require("../utils/ApiResponse.utils.js");
-    const axios=require("axios");
+const { asyncHandler } = require("../utils/asyncHandler.utils.js");
+const { ApiError } = require("../utils/ApiError.utils.js");
+const { ApiResponse } = require("../utils/ApiResponse.utils.js");
+const axios = require("axios");
 const fileExtractor = require("../utils/fileExtractor.utils.js");
 
-    const getInfo = asyncHandler(async (req, res) => {
-        const requestUrl = req.body.url;
+const getInfo = asyncHandler(async (req, res) => {
+    const requestUrl = req.body.url;
 
+    const baseUrl = "https://api.github.com/repos/";
+    const customUrl = baseUrl + "faisallyt/cloud-ide/contents";
+    // const response=await axios.get(customUrl);
+    
+    // console.log(response.data);
+    const structure = await fileExtractor(customUrl);
+    console.log(structure);
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                structure,
+                "Folder structure fetched successfully"
+            )
+        );
+});
 
-        const baseUrl="https://api.github.com/repos/";
-        const response=await axios.get(baseUrl+"faisallyt/cloud-ide/contents");
-        console.log("hello");
-        console.log(response.data);
-        if(response.status!=200 ){
-            throw new ApiError("Failed to fetch repository information",400);
-        }
-        const structure=fileExtractor(response.data);
-        return res
-            .status(200)
-            .json(
-                new ApiResponse(
-                    200,
-                    response.data.length,
-                    "System Hang"
-                )
-            );
-    });
-
-    module.exports = {
-        getInfo,
-    };
+module.exports = {
+    getInfo,
+};
--- server/utils/fileExtractor.utils.js
+++ server/utils/fileExtractor.utils.js
@@ -1,24 +1,29 @@
-const array=[];
+const axios = require('axios');
 
-function fileExtractor(directory){
-    for(let i=0;i<directory.length;i++){
-        if(directory[i].type==="file"){
-            const obj={
-                "name":directory[i].name,
-                "path":directory[i].path,
-                "type":"file",
-            }
-            array
-        }
-        else{
-            const obj={
-                "name":directory[i].name,
-                "path":directory[i].path,
-                "type":"directory",
-                "children":fileExtractor(directory[i].children)
-            }
+async function fileExtractor(customUrl) {
+    const response = await axios.get(customUrl);
+    const directory = response.data;
+    const array = [];
+
+    for (let i = 0; i < directory.length; i++) {
+        if (directory[i].type === "file") {
+            const obj = {
+                "name": directory[i].name,
+                "path": directory[i].path,
+                "type": "file",
+            };
+            array.push(obj);
+        } else {
+            const obj = {
+                "name": directory[i].name,
+                "path": directory[i].path,
+                "type": "directory",
+                "children": await fileExtractor(`${customUrl}/${directory[i].name}`)
+            };
+            array.push(obj);
         }
     }
+    return array;
 }
 
 module.exports = fileExtractor;

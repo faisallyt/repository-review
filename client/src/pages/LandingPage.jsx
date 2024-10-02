df4eb8b8c66d4ad8d73e901c56c54943c752bb9f
faisallyt <fdiledu@gmail.com>
2024-10-02 15:50:35 +0530

    Implement product tab

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,14 +1,92 @@
 import { GridBackgroundDemo } from "../components/Background.jsx";
 import Navbar from "../components/Navbar.jsx";
+import { Tabs } from "../components/ui/Tab.jsx";
+import linearWebp from "../assets/linear.webp";
 
 const LandingPage = () => {
   return (
-    <div>
+    <div className="w-screen">
       <Navbar />
-      <div className="absolute top-[0rem] flex justify-center items-center  w-full">
+      <div className="absolute top-[0rem] flex justify-center items-center  w-screen flex-col">
         <GridBackgroundDemo />
+        <div className="bg-black w-screen ">
+          <TabsDemo />
+        </div>
       </div>
     </div>
+  );
+};
+
+function TabsDemo() {
+  const tabs = [
+    {
+      title: "Product",
+      value: "product",
+      content: (
+        <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
+          <p>Product Tab</p>
+          <DummyContent />
+        </div>
+      ),
+    },
+    {
+      title: "Services",
+      value: "services",
+      content: (
+        <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
+          <p>Services tab</p>
+          <DummyContent />
+        </div>
+      ),
+    },
+    {
+      title: "Playground",
+      value: "playground",
+      content: (
+        <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
+          <p>Playground tab</p>
+          <DummyContent />
+        </div>
+      ),
+    },
+    {
+      title: "Content",
+      value: "content",
+      content: (
+        <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
+          <p>Content tab</p>
+          <DummyContent />
+        </div>
+      ),
+    },
+    {
+      title: "Random",
+      value: "random",
+      content: (
+        <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
+          <p>Random tab</p>
+          <DummyContent />
+        </div>
+      ),
+    },
+  ];
+
+  return (
+    <div className="h-[20rem] md:h-[40rem] [perspective:1000px] relative b flex flex-col max-w-5xl mx-auto w-full  items-start justify-start my-40 bg-black">
+      <Tabs tabs={tabs} />
+    </div>
+  );
+}
+
+const DummyContent = () => {
+  return (
+    <img
+      src={linearWebp}
+      alt="dummy image"
+      width="1000"
+      height="1000"
+      className="object-cover object-left-top h-[60%]  md:h-[90%] absolute -bottom-10 inset-x-0 w-[90%] rounded-xl mx-auto"
+    />
   );
 };
 

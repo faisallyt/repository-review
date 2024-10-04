7f667998fb2a30cd1398a10934a2e5c86049e690
faisallyt <fdiledu@gmail.com>
2024-10-04 15:01:43 +0530

    Fix: Dashboard

---
+++ client/src/components/DashboardGrid.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid.jsx";
+import {
+  IconArrowWaveRightUp,
+  IconBoxAlignRightFilled,
+  IconBoxAlignTopLeft,
+  IconClipboardCopy,
+  IconFileBroken,
+  IconSignature,
+  IconTableColumn,
+} from "@tabler/icons-react";
+
+export function BentoGridDemo() {
+  return (
+    <BentoGrid className="w-screen mx-auto h-[30rem]">
+      {items.map((item, i) => (
+        <BentoGridItem
+          key={i}
+          title={item.title}
+          description={item.description}
+          header={item.header}
+          icon={item.icon}
+          className={i === 0 || i === 6 ? "md:col-span-2" : ""}
+        />
+      ))}
+    </BentoGrid>
+  );
+}
+const Skeleton = () => (
+  <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
+);
+const items = [
+  {
+    title: "Git Track",
+    description: "Explore the birth of groundbreaking ideas and inventions.",
+    header: <Skeleton />,
+    icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "Your Profile",
+    description: "Dive into the transformative power of technology.",
+    header: <Skeleton />,
+    icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
+  },
+];
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import FolderStructureForm from "../components/FolderStructureForm";
+import Navbar from "../components/Navbar";
 
 const Home = () => {
   return (
     <div>
+      <Navbar />
       <div className="App-header">
         <h1>Folder Structure Review</h1>
       </div>
